4cf8b66e83d7445cef7f4199c8f329e3d8ac9db5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-06-27 11:35:13 +0200

    Add ButtonSerial

---
+++ seed/ButtonSerial/ButtonSerial.cpp
@@ -0,0 +1,34 @@
+#include "daisy_seed.h"
+
+// Use the daisy namespace to prevent having to type
+// daisy:: before all libdaisy functions
+using namespace daisy;
+
+// Declare a DaisySeed object called hardware
+DaisySeed hw;
+
+int main(void)
+{
+    // Configure and Initialize the Daisy Seed
+    // These are separate to allow reconfiguration of any of the internal
+    // components before initialization.
+    hw.Configure();
+    hw.Init();
+    hw.StartLog(true);
+
+    //Configure and initialize button
+    Switch button1;
+    //Set button to pin 28, to be updated at a 1kHz  samplerate
+    button1.Init(hw.GetPin(28), 1000);
+
+    // Loop forever
+    for(;;)
+    {
+        //Debounce the button
+        button1.Debounce();
+        //If the button is pressed, turn the LED on
+        hw.PrintLine(button1.Pressed() ? "1" : "0");
+        //wait 1 ms
+        System::Delay(1);
+    }
+}
