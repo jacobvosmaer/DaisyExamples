42a3ead255bbcca0eefb882663ab83c21a7732ee
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-06-28 16:30:16 +0200

    Read some data

--- seed/TestADXL313/TestADXL313.cpp
+++ seed/TestADXL313/TestADXL313.cpp
@@ -7,7 +7,35 @@ using namespace daisy;
 // Declare a DaisySeed object called hardware
 DaisySeed hw;
 
-I2CHandle i2c;
+#define ADXL313_ADDRESS 0x1d
+
+bool i2cRead(I2CHandle& i2c, uint8_t reg, uint8_t* data, uint16_t data_size)
+{
+    if(!data_size)
+        return false;
+
+    data[0] = reg;
+    if(i2c.TransmitBlocking(ADXL313_ADDRESS, data, 1, 1000)
+       != I2CHandle::Result::OK)
+        return false;
+
+    if(i2c.ReceiveBlocking(ADXL313_ADDRESS, data, data_size, 1000)
+       != I2CHandle::Result::OK)
+        return false;
+
+    return true;
+}
+
+bool i2cWrite1(I2CHandle& i2c, uint8_t reg, uint8_t data)
+{
+    uint8_t buf[2];
+    buf[0] = reg;
+    buf[1] = data;
+
+    return i2c.TransmitBlocking(ADXL313_ADDRESS, buf, sizeof(buf), 1000)
+           == I2CHandle::Result::OK;
+}
+
 
 int main(void)
 {
@@ -22,18 +50,17 @@ int main(void)
     i2c_conf.mode           = I2CHandle::Config::Mode::I2C_MASTER;
     i2c_conf.pin_config.scl = {DSY_GPIOB, 8};
     i2c_conf.pin_config.sda = {DSY_GPIOB, 9};
+    I2CHandle i2c;
     if(i2c.Init(i2c_conf) != I2CHandle::Result::OK)
         hw.PrintLine("I2C init failed");
-
 
     hw.Init();
     hw.StartLog(true);
 
-    const uint8_t adxl313 = 0x1d;
-    uint8_t       buf[5]  = {0};
+    uint8_t buf[8];
 
-    i2c.TransmitBlocking(adxl313, buf, 1, 1000);
-    i2c.ReceiveBlocking(adxl313, buf, sizeof(buf), 1000);
+    if(!i2cRead(i2c, 0, buf, 5))
+        hw.PrintLine("i2cRead error reading device ID");
 
     hw.PrintLine("DEV_ID0=%02x DEV_ID1=%02x PARTID=%02x REVID=%02x XID=%02x",
                  buf[0],
@@ -42,9 +69,22 @@ int main(void)
                  buf[3],
                  buf[4]);
 
+    i2cWrite1(i2c, 0x2c, 0b1101);    // 800Hz acquisition
+    i2cWrite1(i2c, 0x31, 0b1000);    // Full resolution
+    i2cWrite1(i2c, 0x38, 0b10 << 6); // Put FIFO in Stream mode
+    i2cWrite1(i2c, 0x2d, 0b1000);    // Start measuring
+
     // Loop forever
     for(;;)
     {
         System::Delay(1);
+
+        i2cRead(i2c, 0x30, buf, 8);
+        if(!(buf[0] & 0x80))
+            continue;
+
+        uint8_t  range = buf[1] & 0b11;
+        uint16_t x     = (((uint16_t)buf[3] << 8) | buf[2]) << range;
+        hw.PrintLine("x=%d", x);
     }
 }
